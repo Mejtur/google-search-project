e01a793c6f20b259c80a9dfcc0e8172ca0fcf22e
mejtur <mejturnl@gmail.com>
2020-10-26 21:05:35 +0100

    buttons
    added changing pages trough buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../css/Navbar.css";
+import {MdSearch} from 'react-icons/md';
+import {Link} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import {setSearch} from '../features/searchSlice';
 
-function Navbar() {
+function Navbar({search}) {
+
+    const [searchInput,setSearchInput] = useState(search);
+    const dispatch = useDispatch(setSearch({
+        search:searchInput,
+    }))
+
+    const handleSearch = () => {
+        if(searchInput.length>0){
+            dispatch(setSearch({
+                search: searchInput,
+            }))}
+    }
+
     return (
         <div className="navbar">
-            <p className="navbar__title">NEWS SEARCH</p>
+            <p className="navbar__title">
+                <Link to="/" style={{testDecoration:"none"}}>
+                NEWS SEARCH
+                </Link>
+            </p>
+            <div className="navbar__searchContainer">
+                <input type="text" value={searchInput} onChange={(e)=>{setSearchInput(e.target.value)}}/>
+                <MdSearch className="navbar__searchIcon" onClick={handleSearch}/>
+            </div>
         </div>
     )
 }
--- src/pages/Results.js
+++ src/pages/Results.js
@@ -3,28 +3,39 @@ import Navbar from '../components/Navbar';
 import {useSelector} from 'react-redux';
 import {selectSearch} from '../features/searchSlice';
 import SingleSearch from '../components/SingleSearch';
+import "../css/Results.css";
 
 function Results() {
 
     const [data,setData] = useState([]);
+    const [page,setPage] = useState(1);
     const search = useSelector(selectSearch);
     const apiKey = "06255b46d2c74fc286d8e87b99aebe0f";
-    const page = 1;
     const url = `https://newsapi.org/v2/everything?q=${search}&pageSize=5&page=${page}&apiKey=${apiKey}`;
 
     useEffect(()=>{
         fetch(url).then((response)=>response.json()).then((data)=>setData(data.articles));
     }, [url])
 
-    console.log(data);
-
     return (
         <div>
-            <Navbar />
-            <div className="searchComponents__container">
+            <Navbar search={search}/>
+            <div className="results__container">
                 {data.map((search,id)=>(
                     <SingleSearch key={id} image={search.urlToImage} title={search.title} content={search.content} url={search.url}/>
                 ))}
+            </div>
+            <div className="results__buttons">
+                {page>1 ? 
+                    <button onClick={()=>{setPage(page-1)}} className="results__buttonClick">Previous Page</button>
+                    : <button disabled>Previous Page</button>  
+                }
+                <p className="results__page">{page}</p>
+                {page<20 ?
+                    <button onClick={()=>{setPage(page+1)}} className="results__buttonClick">Next Page</button>
+                    : 
+                    <button disabled>Next Page</button>
+                }
             </div>
         </div>
     )
